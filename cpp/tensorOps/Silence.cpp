c3f0ee61d4898933c61a627e4e3339708362ede6
ankdesh <a.s.deshwal@gmail.com>
2018-04-21 17:27:40 +0530

    Add Silence layer

---
+++ cpp/tensorOps/Silence.cpp
@@ -0,0 +1,17 @@
+#include "tensor.hpp"
+#include "utilities.hpp"
+
+template <typename DataType> 
+Status silence(const Tensor<DataType>& input){
+
+// Nothing to do. Silence layers just accepts data and
+// doesnt do anything. Not useful outside caffe.
+
+}
+
+
+int main(){
+
+
+}
+
