464bedea4f54ba20faa8c5eef2a7bce416dc6f0d
ankdesh <a.s.deshwal@gmail.com>
2022-11-15 13:53:18 +0530

    Test passes with command sbt test

--- hdl/chisel/chisel-book/Chap4-Components/MyChiselTemplate/src/test/scala/abc/ABCSpec.scala
+++ hdl/chisel/chisel-book/Chap4-Components/MyChiselTemplate/src/test/scala/abc/ABCSpec.scala
@@ -4,7 +4,7 @@ package abc
 
 import chisel3._
 import chiseltest._
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import chisel3.experimental.BundleLiterals._
 import abc._
 
@@ -19,15 +19,17 @@ import abc._
   * sbt 'testOnly gcd.GcdDecoupledTester'
   * }}}
   */
-class ABC extends AnyFreeSpec with ChiselScalatestTester {
+class ABCTest extends AnyFlatSpec with ChiselScalatestTester {
 
-  "ABC should learn to behave" in {
+  "ABCTest" should "pass" in {
     test(new CompC()) { dut =>
       dut.io.a.poke(1.U)
       dut.io.b.poke(2.U)
       dut.clock.step()
       
-      println("Out = " + dut.io.y.peek().toString())
+      // println("Out = " + dut.io.y.peek().toString())
+
+      dut.io.y.expect(3.U)
     }
   }
 }
