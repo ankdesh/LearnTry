ba78297131966f17449dd3219cc73f9effce77a6
ankdesh <a.s.deshwal@gmail.com>
2023-01-10 15:17:40 +0530

    Start adding SST framework

---
+++ cpp/neural-sim/scripts/sst-source-asd.sh
@@ -0,0 +1,9 @@
+export SST_CORE_HOME=/home/ankdesh/explore/sst-sim/sst-core/sst-core-src/
+export SST_CORE_INSTALL=$SST_CORE_HOME/../sst-core-install/
+
+export SST_ELEMENTS_HOME=/home/ankdesh/explore/sst-sim/sst-elements-root/sst-elements
+export SST_ELEMENTS_INSTALL=$SST_ELEMENTS_HOME/../sst-elements-install
+
+export PKG_CONFIG_PATH=$SST_CORE_INSTALL/lib/pkgconfig
+
+export PATH=$PATH:$SST_CORE_INSTALL/bin:$SST_ELEMENTS_INSTALL/bin
