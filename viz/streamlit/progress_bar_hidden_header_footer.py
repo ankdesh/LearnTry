7514be34937c48f51064f94a5db1c7fa3429f568
ankdesh <a.s.deshwal@gmail.com>
2023-09-18 23:08:52 +0530

    Start Learning Streamlit

---
+++ viz/streamlit/progress_bar_hidden_header_footer.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import time
+
+hide_st_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
